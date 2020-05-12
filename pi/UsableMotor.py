27f6dbdc994f450ee8b015c2af221554134d9c16
raho <raho1501@student.miun.se>
2020-05-12 12:44:04 +0200

    cleaned up some redundant code and added light documentation

--- pi/UsableMotor.py
+++ pi/UsableMotor.py
@@ -1,20 +1,23 @@
 from adafruit_motorkit import MotorKit
 from time import sleep
 from enum import Enum
-
+#Motorkit class for simpler use
 class UsableMotors:
     LEFT = 1
     RIGHT = 2
+    #Constructor, maps motorkit output ports on raspi-hat for control. 
+    #put left motor on output 1 and right motor on output 2 when making connections.
     def __init__(self,Adafruit_kit):
         self.LeftMotor = Adafruit_kit.motor1
         self.RightMotor = Adafruit_kit.motor2
-
+    #Set throttle to motor, range is [-1.0; 1.0], negative values results in a backwards spin. 
+    # Throttle is in float.
     def set_throttle(self,selected_motor, throttle_f):
         if selected_motor == self.LEFT:
             self.LeftMotor.throttle = throttle_f
         elif selected_motor == self.RIGHT:
             self.RightMotor.throttle = throttle_f
-
+    #Stopping function, 
     def terminate(self):
         self.LeftMotor.throttle = 0
         self.RightMotor.throttle = 0
--- pi/udp.py
+++ pi/udp.py
@@ -15,20 +15,17 @@ UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 # Bind to address and ip
 UDPServerSocket.bind((localIP, localPort))
 print("UDP server Bound and ready...")
-#start gstreamer as subprocess
 
-process = subprocess.Popen(["sh", "./start_stream_ip_is_viewer.sh"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-print("launched gstreamer")
 #set up motor bindings
-
 kit = MotorKit()
-motors = m_s.UsableMotors(kit)
+motors = m_s.UsableMotors(kit) #use abstracted motor class
 motors.set_throttle(motors.LEFT, 0.0)
 motors.set_throttle(motors.RIGHT, 0.0)
 print("Motors ready...")
-# Listen for incoming datagrams
+
+# Function to listen for incoming datagrams
 def pingresp():
-    IP     = "0.0.0.0"
+    IP     = "0.0.0.0" #accept all connections
     port   = 20003
     buffer  = 1024
     response       = "PONG"
@@ -49,41 +46,28 @@ def pingresp():
 
     
 def main():
-    print("niceness: ", os.nice(0))
-    os.nice(-10)
-    print("new niceness: ", os.nice(0))
+    #main loop for motors
     while(True):
         try:
             bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
             message = bytesAddressPair[0]
             address = bytesAddressPair[1]
-            #clientMsg = "Message from Client:{}".format(message)
-            #clientIP  = "Client IP Address:{}".format(address)
-            #print(clientMsg)
-            #print(message.decode())
-            #print(clientIP)
             data = message.decode()
-            data = data[0:-1]
-            #print(data)
-            actions = json.loads(data)
-            #was thinking if using bumpers on xbox-controller for throttle
-            #and a reverse button
+            data = data[0:-1] #Remove null-str from c-input c++ code sends null terminated c-string
+            actions = json.loads(data) #Load json to dict
             motors.set_throttle(motors.LEFT, actions['l'])
             motors.set_throttle(motors.RIGHT, actions['r'])
         
         except(KeyboardInterrupt):
             break
-
-    process.send_signal(signal.SIGINT)# send Ctrl-C signal
-    stdout, stderr = process.communicate()
-    print( )
-    print("sent sigint to gstreamer: ", stderr.decode() + ";" + stdout.decode())
+    #Loop exit, stop motors
     motors.terminate()
     print("engines stopped")
+    #Close socket
     UDPServerSocket.close()
     print("Socket closed. Exiting program...")
 
 if __name__ == '__main__':
-    t = Thread(target = pingresp, args =(),daemon=True)
-    t.start()
-    main()
+    t = Thread(target = pingresp, args =(),daemon=True) #Thread for ping response function
+    t.start() #Start thread
+    main() # Run main loop
